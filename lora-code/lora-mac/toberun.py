f178dac52cd9b88b3cce82c0a86ec92f0c1f57a7
Pietro <manzoni@ieee.org>
2017-10-26 18:28:36 +0200

    improved version

--- lora-code/lora-mac/toberun.py
+++ lora-code/lora-mac/toberun.py
@@ -2,13 +2,19 @@ from network import LoRa
 import socket
 import machine
 import time
+import binascii
+import network
 
 # Name of your team
-team_name = 'Pietro'
+team_name = 'TeamA'
 
 # initialize LoRa in LORA mode
 # more params can also be given, like frequency, tx power and spreading factor
 lora = LoRa(mode=LoRa.LORA)
+
+# get LoRa MAC address
+loramac = binascii.hexlify(network.LoRa().mac())
+print(loramac)
 
 # create a raw LoRa socket
 s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
@@ -16,12 +22,18 @@ s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 while True:
     # send some data
     s.setblocking(True)
-    s.send('Hello from {}'.format(team_name))
+    tbs = 'Hello from'+team_name
+    print('Sending...'+tbs)
+    s.send(tbs)
 
     # get any data received...
     s.setblocking(False)
     data = s.recv(64)
-    print(data)
+    if data != b'':
+        print(data)
+        LoraStats = lora.stats()            # get lora stats (data is tuple)
+        print(LoraStats)
 
     # wait a random amount of time
-    time.sleep(machine.rng() & 0x0F)
+    rat = machine.rng() & 0x05
+    time.sleep(rat)
