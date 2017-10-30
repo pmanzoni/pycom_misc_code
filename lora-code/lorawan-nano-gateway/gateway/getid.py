8094697e9cfaddfd2b572c62ecc2c2f9a533df3e
pmanzoni <manzoni@ieee.org>
2017-10-30 10:07:07 +0100

    new file to determine ID

---
+++ lora-code/lorawan-nano-gateway/gateway/getid.py
@@ -0,0 +1,8 @@
+import machine
+import binascii
+
+WIFI_MAC = binascii.hexlify(machine.unique_id()).upper()
+# Set  the Gateway ID to be the first 3 bytes of MAC address + 'FFFE' + last 3 bytes of MAC address
+GATEWAY_ID = WIFI_MAC[:6] + "FFFE" + WIFI_MAC[6:12]
+
+print(GATEWAY_ID)
